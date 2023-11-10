e57544b9e1c0df712092e72ced7a9ae99848d300
bishal sen <bishaldazaisen@gmail.com>
2023-11-10 18:36:59 +0530

    initial commit

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,37 @@
+import {IconType } from 'react-icons'
+
+interface AuthSocialButtonProps{
+    icon: IconType,
+    onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+    icon: Icon,
+    onClick
+}) =>{
+    return(
+       <button
+       type="button"
+       onClick={onClick}
+       className="
+       inline-flex
+       w-full
+       justify-center
+       rounded-md
+       bg-white
+       px-4
+       py-2
+       text-stone-300
+       shadow-sm
+       ring-1
+       ring-inset
+       ring-stone-400
+       hover:bg-stone-400
+       focus:outline-offset-0
+       "> 
+       <Icon />
+       </button>
+    );
+}
+
+export default AuthSocialButton;
